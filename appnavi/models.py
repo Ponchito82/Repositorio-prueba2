1899d8f81bc95f372c86edc8c0007ec269a9cd1b
ivan <ivancambron12@gmail.com>
2026-03-25 11:36:35 -0600

    creacion de modelo

--- appnavi/models.py
+++ appnavi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Navegacion(models.Model):
+    nombre = models.CharField(max_length=100)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.nombre
